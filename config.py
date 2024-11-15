f5a478fa0c4c9634b12134205c9d72ca62504daa
𝕯𝖗𝖆𝖌𝖔𝖓🐉 <dragonex7778@gmail.com>
2024-11-15 17:44:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7458456399:AAHZEtllb2IugkwDPL2voyJqZzoE7R1HNbw")
+    API_ID = int(os.environ["API_ID", 20648149]
+    API_HASH = os.environ["API_HASH", "7412daf88182d54faaffb49eb02ab2bb"]
+    AUTH_USERS = "5128979564"
+# 
